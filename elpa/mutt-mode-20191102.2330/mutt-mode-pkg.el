61e6eaea47ba8befa05a6e4f8cfeb8335d5816d3
Kamlesh Gurudasani <kamlesh@ti.com>
2023-04-24 15:50:22 +0530

    Add project to Bitbucket example

---
+++ elpa/mutt-mode-20191102.2330/mutt-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mutt-mode-20191102.2330/mutt-mode.el  -*- no-byte-compile: t -*-
+(define-package "mutt-mode" "20191102.2330" "major mode for editing mutt configuration" '((emacs "24")) :commit "1d495de49e6f536459b00d5396a2f5ce5ad4757b" :authors '(("Felix Weilbach" . "felix.weilbach@t-online.de")) :maintainer '("Felix Weilbach" . "felix.weilbach@t-online.de") :keywords '("languages") :url "https://gitlab.com/flexw/mutt-mode")
---
+++ transient/history.el
@@ -0,0 +1,29 @@
+((magit-am nil
+	   ("--3way"))
+ (magit-blame
+  ("-w"))
+ (magit-branch nil)
+ (magit-cherry-pick
+  ("--ff"))
+ (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-file-dispatch nil)
+ (magit-format-patch:--output-directory)
+ (magit-format-patch:--subject-prefix "tiL5.10-P PATCH v3" "tiL5.10-P RFC PATCH" "tiL5.10-P PATCH")
+ (magit-log
+  ("-n256"
+   ("--" "drivers/crypto/ti/mcrc.c")
+   "--graph" "--decorate"))
+ (magit-patch nil)
+ (magit-patch-apply nil)
+ (magit-patch-create
+  ("--subject-prefix=tiL5.10-P PATCH v3" "--cover-letter" "--output-directory=/home/kamlesh/am62/mcrc/ti-linux-kernel/v35/")
+  ("--subject-prefix=tiL5.10-P RFC PATCH" "--cover-letter" "--output-directory=/home/kamlesh/am62/mcrc/ti-linux-kernel/rfc/")
+  ("--subject-prefix=tiL5.10-P PATCH"))
+ (magit-pull nil)
+ (magit-rebase
+  ("--autostash")
+  nil)
+ (magit-reset nil)
+ (magit-revert nil)
+ (magit:-- "drivers/crypto/ti/mcrc.c" ""))
