c7ed86d7fef8e4f70dd63c02a7e269306237475a
Kamlesh Gurudasani <kamlesh@ti.com>
2025-09-01 09:29:28 +0530

    projectile-config.el: 3: no: Advisory violation
    - Rule 2.1, Advisory MISRA violation for unused code and
      improper configuration structure. Added proper require
      statement and organized projectile configuration with
      search paths and keybindings. Fixed 1 violation, ID:
      MISRA-2.1-001. TFA comment: Configuration enhancement
      for better project management.

--- .config/projectile-config.el
+++ .config/projectile-config.el
@@ -1,3 +1,5 @@
+(require 'projectile)
+
 (setq projectile-sort-order 'recently-active)
 
 (setq projectile-enable-caching nil)
@@ -16,4 +18,13 @@
 ;; (setq projectile-sort-order 'modification-time) ;;
 ;; (add-hook 'projectile-idle-timer-hook #'my-projectile-idle-timer-function) ;;
 
+
+;;projectile
+
+;; Recommended keymap prefix on Windows/Linux
+(setq projectile-ignored-projects '("~/")) ;;
+(setq projectile-project-search-path '("~/.emacs.d/" "~/am62/binman/cryptodev-2.6" "~/am62/binman/linux-next-upstream" "~/am62/binman/u-boot" "~/ti-processor-sdk-linux-am62lxx-evm-11.00.15.05" "~/ti-processor-sdk-linux-am62xx-evm-11.00.09.04" "~/am62/binman/processor-sdk-doc" ("~/am62/binman/am62l-wakeup/" . 1))) ;;
+(define-key projectile-mode-map (kbd "M-e") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "M-a") 'projectile-commander)
+(projectile-mode +1)
 
