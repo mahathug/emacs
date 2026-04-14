883672964f48d7b56e031168356c091257eeedfc
Kamlesh Gurudasani <kamlesh@ti.com>
2026-04-14 16:22:16 +0530

    projectile add patch search

--- .config/projectile-config.el
+++ .config/projectile-config.el
@@ -25,6 +25,35 @@
 
 ;; Recommended keymap prefix on Windows/Linux
 (setq projectile-ignored-projects '("~/")) ;;
+
+(defun projectile-add-search-path (path)
+  "Add PATH to projectile-project-search-path permanently.
+If PATH already exists in the list, it is not added again.
+Updates both the in-memory list and the config file."
+  (interactive "DAdd project search path: ")
+  (if (member path projectile-project-search-path)
+      (message "%s already exists in projectile-project-search-path" path)
+    ;; Add to in-memory list
+    (setq projectile-project-search-path
+          (append projectile-project-search-path (list path)))
+    ;; Anchor on (define-key which always follows the setq list
+    (let ((config-file (expand-file-name "~/.emacs.d/.config/projectile-config.el")))
+      (with-temp-buffer
+        (insert-file-contents config-file)
+        (if (search-forward "\n(define-key projectile-mode-map" nil t)
+            (progn
+              (beginning-of-line)
+              (backward-char 1)
+              (let ((eol (point)))
+                (goto-char eol)
+                (if (search-backward "))" (line-beginning-position) t)
+                    (progn
+                      (insert (format "\n                                        %S" path))
+                      (write-region (point-min) (point-max) config-file))
+                  (message "Could not find )) in closing line"))))
+          (message "Could not find anchor in config file"))))
+    (message "Added '%s' to projectile-project-search-path" path)))
+
 (setq projectile-project-search-path '("~/.emacs.d/"
 				       "~/best-practice/"
 				       "~/cryptohub/"
@@ -47,8 +76,8 @@
 					"~/sdk-yocto"
 				        "~/am62/binman/optee_projects/optee_qemu_master/ftpm-rs/"
 					("~/am62/binman/optee_projects/" . 2) ;;
-                                        ("~/am62/binman/am62l-wakeup/" . 1))) ;;
+                                        ("~/am62/binman/am62l-wakeup/" . 1)
+                                        "~/trusted-firmware-cve/"))
 (define-key projectile-mode-map (kbd "M-e") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "M-a") 'projectile-commander)
 (projectile-mode +1)
-
